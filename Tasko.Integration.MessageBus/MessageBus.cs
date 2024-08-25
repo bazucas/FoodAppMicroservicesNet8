41910fb86e6e30f2fecdb76f332571f9799f210a
Luís Inácio <lrs.inacio@gmail.com>
2024-08-25 22:20:22 +0100

    MessageBus integration project

---
+++ Tasko.Integration.MessageBus/MessageBus.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Tasko.Integration.MessageBus;
+
+public class MessageBus : IMessageBus
+{
+    private readonly string connectionString =
+        "Endpoint=sb://taskoweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HjoslS58pPHtAULb0tay/jx4Ys0+MO5/R+ASbCcFTG0=";
+
+    public async Task PublishMessage(object message, string? topic_queue_Name)
+    {
+        await using var client = new ServiceBusClient(connectionString);
+
+        ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+        var jsonMessage = JsonConvert.SerializeObject(message);
+
+        ServiceBusMessage finalMessage = new(Encoding
+            .UTF8.GetBytes(jsonMessage))
+        {
+            CorrelationId = Guid.NewGuid().ToString(),
+        };
+
+        await sender.SendMessageAsync(finalMessage);
+
+        await client.DisposeAsync();
+    }
+}
